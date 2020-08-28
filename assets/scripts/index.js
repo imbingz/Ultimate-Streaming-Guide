af25ed6eaa040564d0ceb05811c868cfca8ebd37
Zach LaFleur <zach.lafleur95@gmail.com>
2020-08-27 19:44:55 -0600

    Completely restructured the index.html to use skeleton css instead of materialize. attatched jquery, skeleton, and jsPage. created button el and tested in js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,3 +3,7 @@ const omdbKey = "dd9cc031";
 function omdbAPI() {
     return `http://www.omdbapi.com/?t=${userMovie}&apikey=${apiKey}`
 };
+
+$("#aBtn").click(function() {
+    console.log("Hello!");
+});
