7cb2874ec2c4207e7dfa1d2438f7636100e2a208
imbinz <bingz@me.com>
2020-08-29 10:27:30 -0600

    changed omdbURL form t to s

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,34 +1,33 @@
 // UTELLY INFORMATION AND CALL
 var settings = {
-	"async": true,
-	"crossDomain": true,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-		"x-rapidapi-key": "f82a92dbacmsh1aca7fdf76b1beap187f62jsn400f4f407b76"
+	async: true,
+	crossDomain: true,
+	method: 'GET',
+	headers: {
+		'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+		'x-rapidapi-key': 'f82a92dbacmsh1aca7fdf76b1beap187f62jsn400f4f407b76'
 	}
-}
-
-function uTellyURL(movieName) {
-    return "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=" + movieName + "&country=uk"
-} 
-
-// OMDB INFORMATION AND CALL
-const omdbKey = "dd9cc031";
-let userInput = $("#user-input");
-function omdbAPI(movie) {
-    return `https://www.omdbapi.com/?apikey=${omdbKey}&t=${movie}`
 };
 
-// On click listener for the button to collect the data from omdb and console.log
-$("#aBtn").click(function () {
-    let omdbEndPoint = omdbAPI(userInput.val().trim());
-    $.ajax(omdbEndPoint).then(function (response) {
-        console.log(response);
-    });
-    let uTellyEndPoint = uTellyURL(userInput.val().trim());
-    $.ajax(uTellyEndPoint, settings).then(function (response) {
-        console.log(response);
-    });
-});
+function uTellyURL(movieName) {
+	return 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + movieName + '&country=uk';
+}
 
+// OMDB INFORMATION AND CALL
+const omdbKey = 'dd9cc031';
+let userInput = $('#user-input');
+function omdbAPI(movie) {
+	return `https://www.omdbapi.com/?apikey=${omdbKey}&s=${movie}`;
+}
+
+// On click listener for the button to collect the data from omdb and console.log
+$('#aBtn').click(function() {
+	let omdbEndPoint = omdbAPI(userInput.val().trim());
+	$.ajax(omdbEndPoint).then(function(response) {
+		console.log(response);
+	});
+	let uTellyEndPoint = uTellyURL(userInput.val().trim());
+	$.ajax(uTellyEndPoint, settings).then(function(response) {
+		console.log(response);
+	});
+});
